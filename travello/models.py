777d27a0c6576b7531947c81d591071d67104007
Owk_Mukesh <owku.mukesh@gmail.com>
2026-09-14 21:33:19 +0530

    First Commit

---
+++ travello/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Destination:
+    id: int
+    name: str
+    price: int
+    desc: str
---
+++ travello/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from . models import Destination
+
+# Create your views here.
+def index(request):
+    dest1=Destination()
+    dest1.name="Mumbai"
+    dest1.price=1000
+    dest1.desc="The city never sleeps"
+
+    return render(request,"index.html",{"dest1":dest1})
