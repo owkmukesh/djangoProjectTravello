c2496a69716c77febc6c00b5ed368952424a7c71
Owk_Mukesh <owku.mukesh@gmail.com>
2026-09-14 22:02:22 +0530

    modified the index.html img path to dynamic data by adding the line {% static imgPath as baseURL %} and accessed in the img src

--- travello/views.py
+++ travello/views.py
@@ -7,16 +7,19 @@ def index(request):
     dest1.name="Hyderabad"
     dest1.price=800
     dest1.desc="Biryani First, Sherwani next"
+    dest1.img="static/img/place/1.png"
 
     dest2=Destination()
     dest2.name="Mumbai"
     dest2.price=1000
     dest2.desc="The city never sleeps"
+    dest2.img="static/img/place/2.png"
 
     dest3=Destination()
     dest3.name="Benguluru"
     dest3.price=900
     dest3.desc="Industrial Area"
+    dest3.img="static/img/place/3.png"
 
     dests=[dest1,dest2,dest3]
     return render(request,"index.html",{"dests":dests})
