0dd5dd317594a6d38b26c2284afaa609d1ab911d
Owk_Mukesh <owku.mukesh@gmail.com>
2026-09-16 18:36:40 +0530

    add and fetch data from database

--- project_1/settings.py
+++ project_1/settings.py
@@ -130,3 +130,5 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
--- travello/views.py
+++ travello/views.py
@@ -3,26 +3,5 @@ from . models import Destination
 
 # Create your views here.
 def index(request):
-    dest1=Destination()
-    dest1.name="Hyderabad"
-    dest1.price=800
-    dest1.desc="Biryani First, Sherwani next"
-    dest1.img="static/img/place/1.png"
-    dest1.offer=True
-
-    dest2=Destination()
-    dest2.name="Mumbai"
-    dest2.price=1000
-    dest2.desc="The city never sleeps"
-    dest2.img="static/img/place/2.png"
-    dest2.offer=False
-
-    dest3=Destination()
-    dest3.name="Benguluru"
-    dest3.price=900
-    dest3.desc="Industrial Area"
-    dest3.img="static/img/place/3.png"
-    dest3.offer=False
-
-    dests=[dest1,dest2,dest3]
+    dests=Destination.objects.all()
     return render(request,"index.html",{"dests":dests})
