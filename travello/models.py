d28a7b79a794996ab9505160bdb403a9cab8e5d9
Owk_Mukesh <owku.mukesh@gmail.com>
2026-09-16 17:58:05 +0530

    Database config and connection done

--- travello/models.py
+++ travello/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Destination:
-    id: int
-    name: str
-    price: int
-    desc: str
-    img: str
+class Destination(models.Model):
+
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    desc = models.TextField()
+    img = models.ImageField(upload_to='pics', null=True, blank=True)
+    offer = models.BooleanField(default=False)
--- travello/views.py
+++ travello/views.py
@@ -8,18 +8,21 @@ def index(request):
     dest1.price=800
     dest1.desc="Biryani First, Sherwani next"
     dest1.img="static/img/place/1.png"
+    dest1.offer=True
 
     dest2=Destination()
     dest2.name="Mumbai"
     dest2.price=1000
     dest2.desc="The city never sleeps"
     dest2.img="static/img/place/2.png"
+    dest2.offer=False
 
     dest3=Destination()
     dest3.name="Benguluru"
     dest3.price=900
     dest3.desc="Industrial Area"
     dest3.img="static/img/place/3.png"
+    dest3.offer=False
 
     dests=[dest1,dest2,dest3]
     return render(request,"index.html",{"dests":dests})
