8c31523378d4dc97c95919da3e3e656db49e1d5c
Owk_Mukesh <owku.mukesh@gmail.com>
2026-09-17 07:36:52 +0530

    Passing messages Done

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from django.contrib.auth.models import User, auth
 # Create your views here.
 def register(request):
@@ -9,9 +10,20 @@ def register(request):
         password1=request.POST['password1']
         password2=request.POST['password2']
         email=request.POST['email']
-        user=User.objects.create_user(username=username,first_name=first_name,last_name=last_name,password=password1,email=email)
-        user.save();
-        print("User Created")
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,"Username Taken")
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,"Email Taken")
+                return redirect("register")
+            else:
+                user=User.objects.create_user(username=username,first_name=first_name,last_name=last_name,password=password1,email=email)
+                user.save();
+                print("User Created")
+        else:
+            print('password not matching')
+            return redirect("register")
         return redirect('/')
     else:
         return render(request,"register.html")
